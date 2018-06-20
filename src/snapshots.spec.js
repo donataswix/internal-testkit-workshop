2828f63b9197242870a98887ff39f4410d5bd31d
Donatas Petrauskas <donatas.petr@gmail.com>
2018-06-20 23:07:23 +0300

    Snapshots example.

--- src/snapshots.spec.js
+++ src/snapshots.spec.js
@@ -1,5 +1,32 @@
-describe('Snapshot Testing', () => {
-  it('werks', () => {
+import React, {PureComponent} from 'react';
+import ReactTestRenderer from 'react-test-renderer';
 
+function Link ({url, children}) {
+  return (
+    <a style={{textDecoration: 'none'}} className="link" href={url}>{children}</a>
+  );
+}
+
+class MyLinks extends PureComponent {
+  renderLink({url, text}, i) {
+    return <li key={i}><Link url={url}>{text}</Link></li>;
+  }
+
+  render () {
+    return (
+      <ul>{this.props.links.map(this.renderLink)}</ul>
+    );
+  }
+}
+
+describe.skip('Snapshot Testing', () => {
+  it('just works', () => {
+    const links = [
+      {url: 'https://github.com', text: 'Was a good place'},
+      {url: 'https://gitlab.com', text: 'Is a good place'},
+    ];
+    const renderer = ReactTestRenderer.create(<MyLinks links={links}/>);
+
+    expect(renderer).toMatchSnapshot();
   });
 });

d1c9763d533ed50e9a7125a71cb0c18aba344b94
Donatas Petrauskas <donatas.petr@gmail.com>
2018-06-20 22:58:53 +0300

    Remove implementation.

--- src/react-test-renderer.spec.js
+++ src/react-test-renderer.spec.js
@@ -2,59 +2,26 @@ import React, {PureComponent} from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 
 class MyList extends PureComponent {
-
-  constructor(props) {
-    super(props);
-    this.state = {a: 1};
-    this.changeState = this.changeState.bind(this);
-  }
-
-  componentDidMount() {
-    this.input.focus();
-  }
-
-  componentWillUnmount() {
-    console.log('I am gone!');
-  }
-
-  changeState() {
-    this.setState({a: this.state.a + 1});
-    console.log('state =', this.state);
-  }
-
   render () {
-    console.log('render!');
     return (
-      <div onClick={this.changeState}>
-        <input ref={el => this.input = el}/>
-        <ul>{this.props.items.map((item, i) => (<li key={i}>{item}</li>))}</ul>
-      </div>
+      <ul>{this.props.items.map((item, i) => (<li key={i}>{item}</li>))}</ul>
     );
   }
 }
 
-// https://reactjs.org/docs/test-renderer.html
-
 describe('React Test Renderer', () => {
   let items;
   let renderer;
-  let focus;
 
   beforeEach(() => {
-    focus = jest.fn();
     items = ['hello', 'workshop'];
     renderer = ReactTestRenderer.create(
-      <MyList items={items}/>,
-      {
-        createNodeMock: () => {
-          return {focus};
-        }
-      }
+      <MyList items={items}/>
     );
   });
 
-  it.skip('produces plain object representation of rendered compponent with toJSON() method', () => {
-    expect(renderer.toJSON()).toEqual({
+  it('produces plain object representation of rendered compponent with toJSON() method', () => {
+    expect('?').toEqual({
       type: 'ul',
       props: {},
       children: [
@@ -64,30 +31,18 @@ describe('React Test Renderer', () => {
     });
   });
 
-  it('can mock refs', () => {
-    expect(focus).toBeCalled();
+  it.skip('can mock refs', () => {
   });
 
-  it('produces very detailed object with toTree() method', () => {
-    expect(renderer.toTree()).toMatchObject({
-      nodeType: 'component',
-      type: MyList,
-      props: {
-        items: ['hello', 'workshop']
-      }
-    });
+  it.skip('produces very detailed object with toTree() method', () => {
   });
 
-  it('can find elements', () => {
-    expect(renderer.root.findAllByType('li').length).toBe(2);
+  it.skip('can find elements', () => {
   });
 
-  it('can start unmount cycle', () => {
-    renderer.unmount();
+  it.skip('can start unmount cycle', () => {
   });
 
-  it('changes state synchronously', () => {
-    renderer.toJSON().props.onClick();
-    // renderer.unstable_flushSync(() => renderer.toJSON().props.onClick());
+  it.skip('changes state synchronously', () => {
   });
 });
